d66042d425bdd4bd48c71891548e5ba601e66274
EBPG <156937263+EBPG@users.noreply.github.com>
2024-05-17 09:37:03 +0100

    Update run_scenario.py

--- trigger_scenario/run_scenario.py
+++ trigger_scenario/run_scenario.py
@@ -1,1 +1,15 @@
-##
+import dataikuapi
+import sys
+
+host = sys.argv[1]
+apiKey = sys.argv[2]
+project = sys.argv[3]
+
+client = dataikuapi.DSSClient(host,apiKey )
+test_project = client.get_project(project)
+
+# test_project.DSSProjectGit.pull() #needs testing
+
+scenario = test_project.get_scenario('TEST')
+
+scenario.run_and_wait()
