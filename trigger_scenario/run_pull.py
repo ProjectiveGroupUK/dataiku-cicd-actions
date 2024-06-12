21149067b9d838078f61122cb2bcf9d8c6d7af79
EBPG <156937263+EBPG@users.noreply.github.com>
2024-06-12 16:34:30 +0100

    Update run_pull.py

--- trigger_scenario/run_pull.py
+++ trigger_scenario/run_pull.py
@@ -10,11 +10,12 @@ client = dataikuapi.DSSClient(host,apiKey)
 test_project = client.get_project(project)
 
 branch_to_pull = 'scenarios'
+project_git = dataikuapi.dss.project.DSSProjectGit(client, project)
 
 # Switch to the appropriate branch
 try:
     logging.info(f"Switching to branch {branch_to_pull}...")
-    test_project.git_checkout(branch_to_pull)
+    project_git.checkout(branch_to_pull)
     logging.info(f"Successfully switched to branch {branch_to_pull}.")
 except Exception as e:
     logging.error(f"Error switching to branch {branch_to_pull}: {e}")
@@ -23,7 +24,7 @@ except Exception as e:
 # Pull changes from the remote Git repository
 try:
     logging.info(f"Pulling changes from Git branch {branch_to_pull}...")
-    test_project.git_pull()
+    project_git.pull()
     logging.info(f"Successfully pulled changes from Git branch {branch_to_pull}.")
 except Exception as e:
     logging.error(f"Error pulling changes from Git branch {branch_to_pull}: {e}")
