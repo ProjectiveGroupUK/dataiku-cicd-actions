7fb1c93002cf9186e2c7be3f7d011e6195e275ca
EBPG <156937263+EBPG@users.noreply.github.com>
2024-06-18 15:54:34 +0100

    Update run_pull.py

--- trigger_scenario/run_pull.py
+++ trigger_scenario/run_pull.py
@@ -1,31 +1,31 @@
-import dataikuapi
-import logging
-import sys
+#import dataikuapi
+#import logging
+#import sys
 
-host = sys.argv[1]
-apiKey = sys.argv[2]
-project = sys.argv[3]
+#host = sys.argv[1]
+#apiKey = sys.argv[2]
+#project = sys.argv[3]
 
-client = dataikuapi.DSSClient(host,apiKey)
-test_project = client.get_project(project)
+#client = dataikuapi.DSSClient(host,apiKey)
+#test_project = client.get_project(project)
 
-branch_to_pull = 'scenarios'
-project_git = dataikuapi.dss.project.DSSProjectGit(client, project)
+#branch_to_pull = 'scenarios'
+#project_git = dataikuapi.dss.project.DSSProjectGit(client, project)
 
 # Switch to the appropriate branch
-try:
-    logging.info(f"Switching to branch {branch_to_pull}...")
-    project_git.checkout(branch_to_pull)
-    logging.info(f"Successfully switched to branch {branch_to_pull}.")
-except Exception as e:
-    logging.error(f"Error switching to branch {branch_to_pull}: {e}")
-    raise
+#try:
+#    logging.info(f"Switching to branch {branch_to_pull}...")
+#    project_git.checkout(branch_to_pull)
+#    logging.info(f"Successfully switched to branch {branch_to_pull}.")
+#except Exception as e:
+#    logging.error(f"Error switching to branch {branch_to_pull}: {e}")
+#    raise
 
 # Pull changes from the remote Git repository
-try:
-    logging.info(f"Pulling changes from Git branch {branch_to_pull}...")
-    project_git.pull(branch_to_pull)
-    logging.info(f"Successfully pulled changes from Git branch {branch_to_pull}.")
-except Exception as e:
-    logging.error(f"Error pulling changes from Git branch {branch_to_pull}: {e}")
-    raise
+#try:
+#    logging.info(f"Pulling changes from Git branch {branch_to_pull}...")
+#    project_git.pull(branch_to_pull)
+#    logging.info(f"Successfully pulled changes from Git branch {branch_to_pull}.")
+#except Exception as e:
+#    logging.error(f"Error pulling changes from Git branch {branch_to_pull}: {e}")
+#    raise
