17ab76fe61de4f667d355cf6c51b5578cf604198
EBPG <156937263+EBPG@users.noreply.github.com>
2024-06-19 10:22:43 +0100

    Update run_pull.py

--- trigger_scenario/run_pull.py
+++ trigger_scenario/run_pull.py
@@ -1,5 +1,6 @@
 #import dataikuapi
 #import sys
+#import time
 
 #host = sys.argv[1]
 #apiKey = sys.argv[2]
@@ -10,17 +11,20 @@
 
 #branch_to_pull = 'scenarios'
 #project_git_handle = test_project.get_project_git()
+
 #status = project_git_handle.get_status()
 #print(status)
 
 # Switch to the appropriate branch
-# project_git_handle.checkout(branch_name=branch_to_pull)
+#project_git_handle.checkout(branch_name=branch_to_pull)
 
 # Fetch updates from the remote repository
-# project_git_handle.fetch()
+#project_git_handle.fetch()
 
 # Pull changes from the remote Git repository
-# project_git_handle.pull(branch_name=branch_to_pull)
+#project_git_handle.pull(branch_name=branch_to_pull)
 
-# status = project_git_handle.get_status()
-# print(status)
+#status = project_git_handle.get_status()
+#print(status)
+
+#time.sleep(5)
